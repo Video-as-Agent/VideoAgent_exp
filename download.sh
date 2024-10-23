5602ea8dba9f51c9419cf9a94bce1b29949af35b
Achint Soni <90000554+TrickyJustice@users.noreply.github.com>
2024-10-23 14:10:38 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,18 +1,28 @@
 if [ "$1" = "metaworld" ]; then
-    wget -c https://huggingface.co/Po-Chen/flowdiffusion/resolve/main/ckpts/metaworld/model-24.pt
-    mkdir -p ckpts/metaworld
-    mv model-24.pt ckpts/metaworld/model-24.pt
-    echo "Downloaded metaworld model"
-elif [ "$1" = "metaworld-DA" ]; then
-    wget -c https://huggingface.co/Po-Chen/flowdiffusion/resolve/main/ckpts/metaworld_DA/model-24.pt
-    mkdir -p ckpts/metaworld_DA
-    mv model-24.pt ckpts/metaworld_DA/model-24.pt
-    echo "Downloaded metaworld model"
+    wget -c https://huggingface.co/Trickyjustice/VideoAgent/resolve/main/metaworld/model-305.pt
+    mkdir -p results/mw
+    mv model-305.pt results/mw/model-305.pt
+    echo "Downloaded VideoAgent metaworld model"
+elif [ "$1" = "online" ]; then
+    wget -c https://huggingface.co/Trickyjustice/VideoAgent/resolve/main/metaworld/model-3053083.pt
+    mkdir -p results/mw
+    mv model-3053083.pt results/mw/model-3053083.pt
+    echo "Downloaded VideoAgent-online metaworld model"
+elif [ "$1" = "suggestive" ]; then
+    wget -c https://huggingface.co/Trickyjustice/VideoAgent/resolve/main/metaworld/model-4307.pt
+    mkdir -p results/mw
+    mv model-4307.pt results/mw/model-4307.pt
+    echo "Downloaded VideoAgent-suggestive metaworld model"
 elif [ "$1" = "ithor" ]; then
-    wget -c https://huggingface.co/Po-Chen/flowdiffusion/resolve/main/ckpts/ithor/model-30.pt
-    mkdir -p ckpts/ithor
-    mv model-30.pt ckpts/ithor/model-30.pt
-    echo "Downloaded ithor model"
+    wget -c https://huggingface.co/Trickyjustice/VideoAgent/resolve/main/ithor/thor-402.pt
+    mkdir -p results/thor
+    mv model-402.pt results/thor/model-402.pt
+    echo "Downloaded VideoAgent ithor model"
+elif [ "$1" = "bridge" ]; then
+    wget -c https://huggingface.co/Trickyjustice/VideoAgent/resolve/main/bridge/model-44.pt
+    mkdir -p results/bridge
+    mv model-44.pt results/bridge/model-44.pt
+    echo "Downloaded VideoAgent bridge model"
 else 
-    echo "Options: {metaworld, metaworld-DA, ithor}"
+    echo "Options: {metaworld, online, suggestive, ithor, bridge}"
 fi
