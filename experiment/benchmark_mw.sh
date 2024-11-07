b779802ee1b005cebb3fccc5c04505e8dbe08dda
Achint Soni <90000554+TrickyJustice@users.noreply.github.com>
2024-11-06 22:52:20 -0500

    Update benchmark_mw.sh

--- experiment/benchmark_mw.sh
+++ experiment/benchmark_mw.sh
@@ -1,7 +1,7 @@
 # usage: bash benchmark.sh <GPU_ID> <PATH_TO_BENCHMARK.PY>
 for task in "button-press-topdown-v2-goal-observable" "handle-press-v2-goal-observable" "hammer-v2-goal-observable" "basketball-v2-goal-observable" "button-press-v2-goal-observable"
 do 
-    python benchmark_mw.py --env_name $task --n_exps 25 --ckpt_dir "../ckpts/metaworld" --milestone 305  --result_root "../results/305_VideoAgent_mw" 
+    CUDA_VISIBLE_DEVICES=$1 accelerate launch benchmark_mw.py --env_name $task --n_exps 25 --ckpt_dir "../ckpts/metaworld" --milestone 305  --result_root "../results/305_VideoAgent_mw" 
 done
 
 python org_results_mw.py --results_root "../results/results_VideoAgent_mw" 
