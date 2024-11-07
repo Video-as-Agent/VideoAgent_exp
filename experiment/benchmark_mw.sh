c25c82b9ca33df75702216eba70c36deef70bce4
Achint Soni <90000554+TrickyJustice@users.noreply.github.com>
2024-11-07 01:37:44 -0500

    .

--- experiment/benchmark_mw.sh
+++ experiment/benchmark_mw.sh
@@ -4,4 +4,4 @@ do
     CUDA_VISIBLE_DEVICES=$1 accelerate launch benchmark_mw.py --env_name $task --n_exps 25 --ckpt_dir "../ckpts/metaworld" --milestone 305  --result_root "../results/305_VideoAgent_mw" 
 done
 
-python org_results_mw.py --results_root "../results/results_VideoAgent_mw" 
+python org_results_mw.py --results_root "../results/305_VideoAgent_mw" 
