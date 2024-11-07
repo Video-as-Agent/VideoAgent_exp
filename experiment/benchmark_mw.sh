51ab59c99ffa154f765d1f04c405b591d790088a
Achint Soni <90000554+TrickyJustice@users.noreply.github.com>
2024-11-06 22:51:13 -0500

    Update benchmark_mw.sh

--- experiment/benchmark_mw.sh
+++ experiment/benchmark_mw.sh
@@ -1,5 +1,7 @@
 # usage: bash benchmark.sh <GPU_ID> <PATH_TO_BENCHMARK.PY>
 for task in "button-press-topdown-v2-goal-observable" "handle-press-v2-goal-observable" "hammer-v2-goal-observable" "basketball-v2-goal-observable" "button-press-v2-goal-observable"
 do 
-    python benchmark_mw.py --env_name $task --n_exps 25 --ckpt_dir "../ckpts/metaworld" --milestone 3053083  --result_root "../results/3053083_vlm_replan_update_3" 
+    python benchmark_mw.py --env_name $task --n_exps 25 --ckpt_dir "../ckpts/metaworld" --milestone 305  --result_root "../results/305_VideoAgent_mw" 
 done
+
+python org_results_mw.py --results_root "../results/results_VideoAgent_mw" 
